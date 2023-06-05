bc48fca1ced47c2557f4cafa9958bcad1d1c4042
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-05 23:14:48 +0100

    10. Linked list cycle

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
