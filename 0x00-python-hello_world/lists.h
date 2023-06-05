fbcc4b05b8fc13d219ee2f2965a43e84db0f7d98
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-05 23:41:29 +0100

    11. Hello, write

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *
  */
 typedef struct listint_s
 {
@@ -17,6 +17,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
