e87642d0ebcb325a9e4c270dba50e076f657fd7b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-26 17:24:57 +0100

    6. Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    '''Raises a type exception.
+
+    Args:
+        message (str): message to be printed.'''
+    raise NameError(message)
