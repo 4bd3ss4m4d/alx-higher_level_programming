36ca32f6a35f16eb77ade9209a945ad576274e7c
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-27 17:55:08 +0100

    5. Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+'''Define a class Square'''
+
+
+class Square:
+    '''
+    A class to represent a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__: Initialize a new square.
+        area: Calculate the area of the square.
+        size: Getter function for size attribute.
+        size.setter: Setter function for size attribute.
+        my_print: Print the square to stdout using the # character.
+    '''
+    def __init__(self, size=0):
+        '''
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''
+        Getter function for size attribute.
+
+        Returns:
+            The size of the square.
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+        Setter function for size attribute.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        Returns:
+            None
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''
+        Calculate the area of the square.
+
+        Returns:
+            The area of the square.
+        '''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''
+        Print the square to stdout using the # character.
+
+        Returns:
+            None'''
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
