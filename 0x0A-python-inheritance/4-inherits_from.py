0a74b8930c72e971994a6372911ad90e835de8aa
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-07-11 01:11:58 +0100

    Fixing Task 4 error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-Contains the inherits_from function
+lass that inherited (directly or indirectly) from the specified class
 """
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """
+    returns True if the object is an instance of a class that
+    inherited from the specified class;otherwise False
+    """
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
