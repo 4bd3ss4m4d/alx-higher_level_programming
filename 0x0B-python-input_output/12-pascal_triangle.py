fa0c4f2a54fff1951afe5df63f412b857edfb1d4
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-07-13 00:42:31 +0100

    Fixing task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""lists of integers representing the Pascal’s triangle of n"""
+""" pascal triangle"""
 
 
-def pascal_triangle(n):
-    if n <= 0:
-        return []
+def pascal_triangle(n=4500):
+    """print pascal"""
+    pascal = [[0]*i for i in range(1, n+1)]
+    cmpt = 0
+    for i in range(n):
+        pascal[i][0] = 1
+        pascal[i][-1] = 1
+        for j in range(0, i//2):
+            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
+            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+    return pascal
